29a0781f96e4ef925dbe003419abafdec612de6b
nicolealisachan <ncha756@aucklanduni.ac.nz>
2016-05-26 12:29:16 +1200

    Add files via upload

--- Sketch.cs
+++ Sketch.cs
@@ -8,19 +8,24 @@ public class Sketch : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		for ( int i = 0; i < 3; i++ )
-		{ 
-			float x = 3.0f; 
+		int totalCubes = 8; 
+
+		int totalDistance = 5; 
+
+		for (int i = 0; i < totalCubes; i++) 
+		{
+			float perc = i / (float)totalCubes; 
+
+			float x = perc * totalDistance; 
 			float y = 5.0f; 
 			float z = 0.0f; 
 
-			var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity); 
-			newCube.GetComponent<CubeScript>().SetSize(1.5f); 
+			var newCube = (GameObject)Instantiate (myPrefab, new Vector3 (x, y, z), Quaternion.identity); 
+			newCube.GetComponent<CubeScript>().SetSize (1.0f- perc);
+			newCube.GetComponent<CubeScript>().rotateSpeed = 1.0f- perc; // Random.value; 
 
+		}
 	}
-
-
-
 	
 	// Update is called once per frame
 	void Update () {
